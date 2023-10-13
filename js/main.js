cbe11c086d9ffd6bcf5600dab20d8a642f20beca
seoyoona <synsyn0311@gmail.com>
2023-10-14 04:04:28 +0900

    layout_final

--- js/main.js
+++ js/main.js
@@ -1,3 +1,4 @@
+// lang_bar
 $(function () { 
     $('[data-toggle="tooltip"]').tooltip({trigger: 'manual'}).tooltip('show');
   });  
@@ -11,3 +12,51 @@ $(function () {
          
    //  }  
   // });
+
+// tab_btn
+const tabWrapper = document.querySelectorAll('.tab_wrap');
+function singleTab() {
+	const tabContent = document.querySelectorAll('.tab_content>div');
+	const targetLink = document.querySelectorAll('.tab_btn a');
+	for (let i = 0; i < targetLink.length; i++) {
+		targetLink[i].addEventListener('click', (e) => {
+			e.preventDefault();
+
+			for (let j = 0; j < targetLink.length; j++) {
+				targetLink[j].classList.remove('active');
+				e.target.classList.add('active');
+			}
+			let orgTarget = e.target.getAttribute('href');
+			console.log(orgTarget);
+			for (let x = 0; x < tabContent.length; x++) {
+				tabContent[x].style.display = 'none';
+			}
+			document.querySelector(orgTarget).style.display = 'block';
+		});
+		document.querySelector('#tabs-1').style.display = 'block';
+	}
+}
+
+for (const el of tabWrapper) {
+	const targetLink = el.querySelectorAll('ul.tab_btn a');
+	let tabContent = el.querySelectorAll('.tab_content > div');
+	targetLink.forEach(function (el) {
+		el.addEventListener('click', function (e) {
+			e.preventDefault();
+			let tg = this;
+			let currentLink = tg.getAttribute('href');
+			controlClass(targetLink);
+			controlClass(tabContent);
+			document.querySelector(currentLink).classList.add('active');
+			tg.classList.add('active');
+		});
+	});
+}
+
+function controlClass(old) {
+	let elements = Array.from(old);
+	elements.forEach(function (elem) {
+		elem.classList.remove('active');
+	});
+}
+
