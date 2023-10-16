1d5f265c8a1ab5058b1cf427f3198eaca920a0b6
seoyoona <synsyn0311@gmail.com>
2023-10-16 10:39:21 +0900

    edited

--- js/main.js
+++ js/main.js
@@ -1,14 +1,14 @@
 // lang_bar
-$(function () { 
-    $('[data-toggle="tooltip"]').tooltip({trigger: 'manual'}).tooltip('show');
-  });  
+// $(function () { 
+//     $('[data-toggle="tooltip"]').tooltip({trigger: 'manual'}).tooltip('show');
+//   });  
   
-  // $( window ).scroll(function() {   
-   // if($( window ).scrollTop() > 10){  // scroll down abit and get the action   
-    $(".progress-bar").each(function(){
-      each_bar_width = $(this).attr('aria-valuenow');
-      $(this).width(each_bar_width + '%');
-    });
+//   // $( window ).scroll(function() {   
+//    // if($( window ).scrollTop() > 10){  // scroll down abit and get the action   
+//     $(".progress-bar").each(function(){
+//       each_bar_width = $(this).attr('aria-valuenow');
+//       $(this).width(each_bar_width + '%');
+//     });
          
    //  }  
   // });
