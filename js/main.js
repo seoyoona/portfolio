ce72ff267474d4a7f1f648c88637f2191717b6f5
seoyoona <synsyn0311@gmail.com>
2023-10-19 21:20:17 +0900

    device js

--- js/main.js
+++ js/main.js
@@ -60,3 +60,74 @@ function controlClass(old) {
 	});
 }
 
+// projects
+const win = $(window);
+const sections = $('.project_wrap');
+let speed = Math.floor(win.height() * 0.5);
+let topArr = [];
+let winSCT;
+console.log(speed);
+//sections.offsetTop
+sections.each(function (i, o) {
+	const sectionTop = $(o).offset().top;
+	topArr.push(sectionTop);
+});
+win.on('scroll', () => {
+	winSCT = win.scrollTop();
+	if (winSCT > topArr[0] && winSCT < topArr[1]) {
+		sections.eq(0).addClass('is-animated').siblings().removeClass('is-animated');
+	}
+	if (winSCT > topArr[1] - speed && winSCT < topArr[2]) {
+		sections.eq(1).addClass('is-animated').siblings().removeClass('is-animated');
+	}
+	if (winSCT > topArr[2] - speed && winSCT < topArr[3]) {
+		sections.eq(2).addClass('is-animated').siblings().removeClass('is-animated');
+		pipScroll();
+	}
+	if (winSCT > topArr[3] - speed && winSCT < topArr[4]) {
+		sections.eq(3).addClass('is-animated').siblings().removeClass('is-animated');
+		pipScroll();
+	}
+	if (winSCT > topArr[4] - speed) {
+		sections.eq(4).addClass('is-animated').siblings().removeClass('is-animated');
+		pipScroll();
+		console.log(topArr[4],winSCT);
+	}
+});
+function pipScroll(params) {
+	const devices = ['.mockup.pc', '.mockup.mobile','.mockup.tablet'];
+	$.each(devices, function (i, deviceEl) {
+		const device = $(deviceEl);
+		const screen = device.find('.screen');
+		const mask = device.find('.mask');
+		const hightDifference = screen.innerHeight() - mask.innerHeight();
+		console.log(hightDifference);
+		device.on({
+			mouseenter: function () {
+				screen.stop().animate({ top: -hightDifference }, 1000);
+			},
+			mouseleave: function () {
+				screen.stop().animate({ top: 0 }, 1000);
+			},
+		});
+	});
+}
+win.on('resize', function () {
+	pipScroll();
+});
+
+
+// modal
+$(document).ready(function() {
+    $('.tab_photo').on('click', function() {
+        $('#modal').css('display', 'block');
+    });
+
+    $('#modal').on('click', function() {
+        $(this).css('display', 'none');
+    });
+
+	$('.modal_close_btn').on('click',function(){
+		$('#modal').css('display','none')
+	})
+});
